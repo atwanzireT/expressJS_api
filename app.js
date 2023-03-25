581b520ce9756748bbbf0e4fc60d739b65d59403
atwanzireT <tatwanzire@gmail.com>
2023-03-25 23:23:44 +0000

    routes

--- app.js
+++ app.js
@@ -6,6 +6,20 @@ app.get('/', (req, res) => {
     res.send("Hello World");
 });
 
+app.get('/about', (req, res) => {
+    res.send("This is the about page .");
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`);
 });
+
+// POST method route
+app.post('/', (req, res) => {
+    res.send('POST request to the homepage')
+})
+
+app.all('/secret', (req, res, next) => {
+    console.log('Accessing the secret section ...')
+    next() // pass control to the next handler
+})
