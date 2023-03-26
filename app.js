08a896f6ab067f70d8fad54cdc91732ac1c2ac51
atwanzireT <tatwanzire@gmail.com>
2023-03-26 20:41:19 +0100

    more routes

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req, res) => {
     res.send("This is the about page .");
-})
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`);
@@ -16,10 +16,25 @@ app.listen(port, () => {
 
 // POST method route
 app.post('/', (req, res) => {
-    res.send('POST request to the homepage')
-})
+    res.send('POST request to the homepage');
+});
 
 app.all('/secret', (req, res, next) => {
     console.log('Accessing the secret section ...')
     next() // pass control to the next handler
+});
+
+// route paths based on string patterns
+app.get('/ab?cd', (req, res, next) => {
+    res.send("ab?cd");
+});
+
+// This route path will match abcd, abbcd, abbbcd
+app.get('/ab+cd', (req, res) => {
+  res.send('ab+cd');
+});
+
+// This route path will match /abe and /abcde.
+app.get('/ab(cd)?e', (req, res) => {
+  res.send('ab(cd)?e')
 })
